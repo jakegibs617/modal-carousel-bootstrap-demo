11c42c37457f8bfbfc0b773a2d1c47cabbab5785
Jacob <jakegibs617@gmail.com>
2015-11-23 17:32:38 -0500

    cleaned footer

--- footer.php
+++ footer.php
@@ -1,6 +1,11 @@
+	<!-- jquery.min -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+
+    <!-- bootstrap.min -->
     <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
+    <!-- bootsrtap modal carousel lib -->
     <script src="./js/bootstrap-modal-carousel.min.js"></script>
-    <script src="./js/demo.min.js"></script>
+
   </body>
 </html>
