21ad8a9e8712d68eaf8e0bd9d157943460c2de62
Jacob <jakegibs617@gmail.com>
2015-11-23 17:28:41 -0500

    clean organization

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Bootstrap modal carousel : modal-carousel </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/modal-carousel.css">
+    <link rel="stylesheet" href="./css/style.css">
+
+  </head>
--- index.php
+++ index.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Bootstrap modal carousel : modal-carousel </title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./css/modal-carousel.css">
-    <link rel="stylesheet" href="./css/style.css">
-
-  </head>
-
+<?php include 'header.php'; ?>
   <body>
 
     <button type="button" class="btn btn-info" data-toggle="modal" data-target="#myModal" data-local="#myCarousel">
